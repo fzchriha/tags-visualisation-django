82bac575c5932dc799c848129507fca8cdfced11
Fatima Zahra Chriha <fatimazahra.chriha@gmail.com>
2020-06-08 23:02:58 -0400

    Intial Commit

---
+++ posts/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from .forms import PostForm
+from collections import Counter
+from taggit.models import Tag
+from django.template.defaultfilters import slugify
+import json
+
+def home_view(request):
+    posts = Post.objects.all()
+    common_tags = Post.tags.most_common()[:4]
+    form = PostForm(request.POST)
+    if form.is_valid():
+        newpost = form.save(commit=False)
+        newpost.slug = slugify(newpost.title)
+        newpost.save()
+        form.save_m2m()
+    context = {
+        'posts':posts,
+        'common_tags':common_tags,
+        'form':form,
+    }
+    return render(request, 'home.html', context)
+
+def detail_view(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    context = {
+        'post':post,
+    }
+    return render(request, 'detail.html', context)
+
+def tagged(request, slug):
+    tag = get_object_or_404(Tag, slug=slug)
+    common_tags = Post.tags.most_common()[:4]
+    posts = Post.objects.filter(tags=tag)
+    context = {
+        'tag':tag,
+        'common_tags':common_tags,
+        'posts':posts,
+    }
+    return render(request, 'home.html', context)
+
+def pie_chart(request):
+    traits = Counter()
+    crushes =  Post.objects.all()
+    for crush in crushes:
+        for trait in crush.tags.names():
+            traits[trait] += 1
+    context = {
+        'traits': dict(traits)
+    }
+    return render(request, 'pie_chart.html', context)
